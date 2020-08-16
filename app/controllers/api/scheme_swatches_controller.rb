e8a417576ef8226051b73e455d189598dedfab07
Eric Pinero <ericpinero@gmail.com>
2020-08-16 13:50:53 -0400

    build scheme swatch controller

--- app/controllers/api/scheme_swatches_controller.rb
+++ app/controllers/api/scheme_swatches_controller.rb
@@ -1,8 +1,25 @@
 class Api::SchemeSwatchesController < ApplicationController
+    def create
+        @scheme_swatch = Scheme_swatch.new(scheme_swatch_params)
+        if @scheme_swatch.save
+            render :show
+        else
+            render json: @scheme_swatch.errors.full_messages, status: 401
+        end
+    end
+
+    def destroy
+        @scheme_swatch = Scheme_swatch.find(params[:id])
+        if @scheme_swatch.destroy
+            render :remove
+        else
+            render json: @scheme_swatch.errors.full_messages, status: 422
+        end
+    end
 
 
     private
     def scheme_swatch_params
-        
+        params.require(:scheme_swatch).permit(:color_id, :scheme_id)
     end
 end
--- app/models/scheme.rb
+++ app/models/scheme.rb
@@ -4,4 +4,16 @@ class Scheme < ApplicationRecord
     has_many :scheme_swatches
     belongs_to :scheme
 
+    def swatch_obj
+        swatch_obj = {}
+
+        for scheme_swatch in scheme_swatches.all do 
+            swatch_obj[scheme_swatch.id] = {
+                color => scheme_swatch.color,
+                complement => scheme_swatch.complement
+            }
+        end
+        swatch_obj
+    end
+
 end
